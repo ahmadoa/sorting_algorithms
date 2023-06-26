1238a8250e08807fe518a6eda8da7828a1cd3e93
ahmadoa <ouladaouida@gmail.com>
2023-06-26 05:23:06 -0700

    removed no more needed function declarations

--- sort.h
+++ sort.h
@@ -22,7 +22,5 @@ void print_list(const listint_t *list);
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
-size_t segragation(int *array, ssize_t min, ssize_t max, size_t size);
-void sorting(int *array, ssize_t str, ssize_t fn, size_t size);
 void quick_sort(int *array, size_t size);
 #endif
